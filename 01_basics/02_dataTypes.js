a0a9c277d0e3c496a6fb49e684ffb576802c4e71
Sawan Solanki <155409451+sawancode@users.noreply.github.com>
2024-06-25 05:20:08 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JavaScript code as newer version
+
+// alert (3+3); // we are suing nodejs, not browser
+
+ console.log(3+3); 
+ console.log("sawan");
+
+
+let name = "sawan";
+let age = 7;
+let isLoggedIn = false;
+
+// premitive data types
+// number range ==> 2 to power 53;
+// bigint
+//string ==>""
+//null ==> stand alone value special type
+//symbol ==> unique
+//undefined ==> 
+//boolean ==> true / false
+
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
