3f74eef331be18835a67a61d9cc6779d670d290a
Sawan Solanki <155409451+sawancode@users.noreply.github.com>
2024-06-26 07:48:28 +0000

    String Methods

---
+++ 01_basics/05_string.js
@@ -0,0 +1,35 @@
+const name = "sawan";
+const repoCount = 25;
+
+// console.log(name+repoCount+"Concat");
+
+// console.log(`Hello my name is ${name} and the repoCount is ${repoCount}`);
+
+const gameName =new String('Sawan');
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length); // find out the length of array.
+// console.log(gameName.charAt(1)); // find out the character with position.
+// console.log(gameName.indexOf('a')); // find out the index with position on character.
+
+// const newString = gameName.substring(0, 3); // give you the character passing with arrguemtn.
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4); // nagative value bhi passed kr skte hen joki reverse me chelga.
+// console.log(anotherString);
+
+const newStringOne = "   sawan    ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim()); // remove the space 
+
+const url = "https://sawan.com/sawan%20solanki";
+// console.log(url.replace('%20', '-')); // replace the character giving syntax
+
+
+
+
+
+
+
+
+
