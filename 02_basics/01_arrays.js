737c1ad303f7eb5ccecdacb1d38127c3917cb6e1
Sawan Solanki <155409451+sawancode@users.noreply.github.com>
2024-07-02 14:49:56 +0000

    Arrays and Methods

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -20,16 +20,20 @@ const newArr = myArr.join();
 
 // slice, splice
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3); // slice method give index ke last element ko include nhi krega.
 
-console.log(myn1);
-console.log("B", myArr);
+// console.log(myn1);
+// console.log("B", myArr);
 
 const myn2 = myArr.splice(1, 3); // original array ko manupulate krta ha
-console.log("C", myArr);
-console.log(myn2);
+// console.log("C", myArr);
+// console.log(myn2);
+
+
+//*******************************************************************
+
 
 
 
