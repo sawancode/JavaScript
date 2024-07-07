931c36e83728ebb8b82ed6bc3eb8ab1bcfcca6b6
Sawan Solanki <155409451+sawancode@users.noreply.github.com>
2024-07-07 06:26:15 +0000

    While and Do-While

---
+++ 05_iteration/two.js
@@ -0,0 +1,20 @@
+
+let i = 0;
+while (i < 2) {
+    // console.log(`value of ${i}`);
+    i++
+}
+
+let myArray = ["hulk", "spiderman", "thor"]
+let arr = 0;
+
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`)
+    arr++;
+}
+
+let score = 1;
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score <= 10);
