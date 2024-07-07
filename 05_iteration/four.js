862931648449bd50000ab977a0259e92d6c75ad9
Sawan Solanki <155409451+sawancode@users.noreply.github.com>
2024-07-07 08:31:32 +0000

    Foor In Loop

---
+++ 05_iteration/four.js
@@ -0,0 +1,20 @@
+// for in loop
+
+const myObject = {
+    js: "JavaScript",
+    cpp: "C++",
+    rb: "ruby",
+    swift: "swift by apple",
+}
+
+for (const key in myObject) {
+    // console.log(`${key} for shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"];
+
+for (const key in programming) {
+    // console.log(`${key} and value is ${programming[key]}`);
+}
+
+//
