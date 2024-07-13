394e46f7dd4a6fa28f28b4093c49fb0e88d01998
Sawan Solanki <155409451+sawancode@users.noreply.github.com>
2024-07-13 03:38:58 +0000

    Filter Uses

--- 05_iteration/six.js
+++ 05_iteration/six.js
@@ -42,5 +42,9 @@ const books = [
   ];
 
 let userBooks = books.filter( (bk) => bk.publish === 'History');
- userBooks = books.filter( (bk) => bk.publish>2000)
-// console.log(userBooks);
+
+ userBooks = books.filter( (bk) => {
+    return bk.publish >= 1995 && bk.genre === "History"
+ })
+console.log(userBooks);
+
